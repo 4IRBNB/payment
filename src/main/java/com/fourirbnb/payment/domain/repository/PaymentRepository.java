7065c84d828f12a86068f47acaca26cd2c17d54f
Jeongwook Woo <dnwjddnr115@naver.com>
2025-04-14 19:03:39 +0900

    feature: 결제 조회 API 구현

--- src/main/java/com/fourirbnb/payment/domain/repository/PaymentRepository.java
+++ src/main/java/com/fourirbnb/payment/domain/repository/PaymentRepository.java
@@ -1,7 +1,10 @@
 package com.fourirbnb.payment.domain.repository;
 
 import com.fourirbnb.payment.domain.model.Payment;
+import java.util.Optional;
 import java.util.UUID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -10,4 +13,10 @@ public interface PaymentRepository {
   void save(Payment payment);
 
   boolean existsByReservationId(UUID reservationId);
+
+  Optional<Payment> findById(UUID paymentId);
+
+  Page<Payment> findAll(Pageable pageable);
+
+  Optional<Payment> findByReservationId(UUID reservationId);
 }
--- src/main/java/com/fourirbnb/payment/infrastructure/repository/PaymentRepositoryImpl.java
+++ src/main/java/com/fourirbnb/payment/infrastructure/repository/PaymentRepositoryImpl.java
@@ -2,8 +2,11 @@ package com.fourirbnb.payment.infrastructure.repository;
 
 import com.fourirbnb.payment.domain.model.Payment;
 import com.fourirbnb.payment.domain.repository.PaymentRepository;
+import java.util.Optional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 @RequiredArgsConstructor
@@ -16,6 +19,24 @@ public class PaymentRepositoryImpl implements PaymentRepository {
   public void save(Payment payment) {
 
     jpaRepository.save(payment);
+  }
+
+  @Override
+  public Optional<Payment> findById(UUID paymentId) {
+
+    return jpaRepository.findById(paymentId);
+  }
+
+  @Override
+  public Optional<Payment> findByReservationId(UUID reservationId) {
+
+    return jpaRepository.findByReservationId(reservationId);
+  }
+
+  @Override
+  public Page<Payment> findAll(Pageable pageable) {
+
+    return jpaRepository.findAll(pageable);
   }
 
   @Override
